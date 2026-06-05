dc095f6a93228007b016c3fc9f4e7ca5a85e2e99
RobotComp.ru <semen228.maslennikov30@gmail.com>
2026-06-05 21:24:40 +0500

    main

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def about():
 @app.route ("/")
 def index():
     print ("bloger mellstroy")
-    return render_template("index.html")
+    return render_template("index.html", name = "mellstroy")
 
 
 
